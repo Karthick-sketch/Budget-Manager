c0296e21ced07fb2df4f3d351c9e2e154df70e9f
Karthick-Ramesh-16 <karthick.ramesh@freshworks.com>
2022-06-18 13:56:33 +0530

    Add budget calculation

--- src/main/java/com/example/budget_manager/controller/BudgetsController.java
+++ src/main/java/com/example/budget_manager/controller/BudgetsController.java
@@ -5,13 +5,27 @@ import com.example.budget_manager.model.Budget;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
 public class BudgetsController {
     @Autowired
     BudgetRepo budgetRepo;
+
+    @GetMapping("/dashboard")
+    public Map<String, Double> getBudgetCalculation() {
+        Double expenses = budgetRepo.sumOfExpenses(), incomes = budgetRepo.sumOfIncomes();
+
+        Map<String, Double> budgetCalculation = new HashMap<>();
+        budgetCalculation.put("expenses", expenses);
+        budgetCalculation.put("incomes", incomes);
+        budgetCalculation.put("balance", incomes-expenses);
+
+        return budgetCalculation;
+    }
 
     @GetMapping("/budgets")
     public List<Budget> getBudgets() {
--- src/main/java/com/example/budget_manager/datarepo/BudgetRepo.java
+++ src/main/java/com/example/budget_manager/datarepo/BudgetRepo.java
@@ -2,6 +2,12 @@ package com.example.budget_manager.datarepo;
 
 import com.example.budget_manager.model.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BudgetRepo extends JpaRepository<Budget, Integer> {
+    @Query(value = "select sum(amount) as Expenses from budgets where budget_type = 'expense'", nativeQuery = true)
+    Double sumOfExpenses();
+
+    @Query(value = "select sum(amount) as Incomes from budgets where budget_type = 'income'", nativeQuery = true)
+    Double sumOfIncomes();
 }
